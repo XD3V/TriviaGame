50650f51b9e08cffd8925dc6580039aeb4832486
Xavier Davis <xdavis1@icloud.com>
2019-08-21 18:05:14 -0700

    added a working timer

--- app.js
+++ app.js
@@ -7,17 +7,34 @@ function secondPg(){
     
    // creating the if stament for hidding and displaying an element when hitting start button
    if (starbtn === false) {
-
-    document.getElementById("container2").style.visibility = "visible";
-
-        console.log("apples");
-
-    document.getElementById("container1").style.display = "none";
+       
+        // changing 
+        document.getElementById("container1").style.display = "none";
 
         console.log("pears");
 
-        document.getElementById("#container3").style.display = "visible";
+        document.getElementById("container2").style.visibility = "visible";
+
+        console.log("apples");
+
+        document.getElementById("container3").style.visibility = "visible";
 
         console.log("banna");
-   };
+
+        // create the start time for the timer
+        var timeLeft = 30;
+         //create the timer interval
+        var  counter = setInterval(function(){
+           //use the id in html document to display and count down the time with a message
+            document.getElementById("container3").innerHTML = "<h2>"+"You have " + timeLeft + " seconds remaining!!!" + "</h2>";
+            timeLeft -= 1;
+
+            // create an if statment for when the timer is done it replaces the other text to display something else.
+            if(timeLeft <= 0){
+              clearInterval(counter);
+              document.getElementById("container3").innerHTML =  "<h2>" + "TIMES UP !!" + "</h2>";
+
+         }   
+      }, 1000); 
+   }
 };
